3472ab361c24511e901dfd67aa9e9a72739bcfd0
unknown <katharina.r@hotmail.fr>
2020-05-20 10:18:42 +0200

    update

--- index.php
+++ index.php
@@ -69,3 +69,112 @@ function showHome() {
     // par exemple $datas["annee"] = 2020;
 	return ["template" => "home.html", "datas" => $datas];
 }
+
+
+// Fonctionnalité(s) redirigées :
+
+function insertUser() {
+    
+    if(!empty($_POST["nom"]) && !empty($_POST["prenom"]) && !empty($_POST["email"]) && !empty($_POST["pseudo"]) &&
+    $_POST["password"] === $_POST["password2"]) {
+      
+        /*if (preg_match("#^[a-zA-Z-àâäéèêëïîôöùûüçàâäéèêëïîôöùûüçÀÂÄÉÈËÏÔÖÙÛÜŸÇæœÆŒ]+$#", $_POST["nom"])
+            && preg_match("#^[a-zA-Z-àâäéèêëïîôöùûüçàâäéèêëïîôöùûüçÀÂÄÉÈËÏÔÖÙÛÜŸÇæœÆŒ]+$#", $_POST["prenom"])
+            && preg_match("#^(a-z0-9)+(a-z0-9)+@(a-z0-9)+(a-z0-9)$#", $_POST["email"])
+            && preg_match("# \^[a-zA-Z0-9_]{3,16}$#", $_POST["pseudo"])
+            && preg_match("#^[a-zA-Z0-9]+$#", $_POST["password"]))  {*/
+
+                $user = new User();
+                $user->setNick($_POST["nick"]);
+                $user->setNom($_POST["nom"]);
+                $user->setPrenom($_POST["prenom"]);
+                $user->setEmail($_POST["email"]);
+                $user->setPassword(password_hash($_POST["password"], PASSWORD_DEFAULT));
+                $user->setAutorisation($_POST["autorisation"]);
+
+                $user->insert();
+                $pseudo= isset($_POST['nick'])? $_POST['nick'] : "null";
+                $password= isset($_POST['password'])? $_POST['password'] : "null";
+                $_SESSION['nick']=$nick;
+                $_SESSION['password']=$password;
+
+        }else {
+                echo "Erreur.<br>";
+        
+        }
+    }
+    setcookie('nick', $_POST['nick'], time() + 182 * 24 * 60 * 60, '/');
+    //header("Location:index.php");
+}  
+
+function connectUser() {
+
+    if(!empty($_POST["nick"]) && !empty($_POST["password"])) {
+
+        $user = new User();
+        $user->setNick($_POST["nick"]);
+        $user->setPassword($_POST["password"]);
+        $verif = $user->selectByNick();
+
+        if($verif) { 
+                if(password_verify($_POST["password"], $verif["password"])) {
+                    $_SESSION["user"] = $verif;
+                    header('Location:index.php?route=membre'); 
+                   
+                } else { 
+                    header('Location:index.php?route=home');
+                } 
+            
+        } else {
+            header('Location:index.php?route=home');
+        }
+    } 
+    
+}
+
+function deconnectUser() {
+    unset($_SESSION["user"]);
+    header("Location:index.php");
+}
+
+function insertBook() {
+
+    if(!empty($_POST["title"]) && !empty($_POST["author"]) && !empty($_POST["description"]) && !empty($_POST["opinion"]) && !empty($_POST["note"])) {
+
+        $book = new Book();
+        $book->setTitle($_POST["title"]);
+        $book->setAuthor($_POST["author"]);
+        $book->setDescription($_POST["description"]);
+        $book->setOpinion($_POST["opinion"]);
+        $book->setNote($_POST["note"]);
+
+        $book->setIdUser($_SESSION['user']['id_user']);
+
+        $book->insert();
+    }
+    
+    header("Location:membre.php");  
+}
+    
+?>
+
+<!--4. TEMPLATE
+Affichage du système de templates HTML-->
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Club de livres</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <?php require "views/{$view['template']}"; ?>
+
+
+</body>
+</html>
